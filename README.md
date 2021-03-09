445ab56cad3df46d65f349de0f6fe933ec4eb60a
William Tao <williamtao345@gmail.com>
2021-03-09 11:12:14 -0500

    Added byteBox.java

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ## Who am I?
 + I am a 9th Grader who have learned C, Python, Javascript and who is learning Java now.
 + I am good at Math(no kidding). I have finished Database by myself last summer-break, and I am taking AP Calculus BC and AP Computer Science A now.
-## Why I created this repository?
-1. Save the work I have done about Java
-2. Record my Java learning process
-
+## Why did I create this repository?
++ Save the work I have done about Java
++ Record my Java learning process
---
+++ src/com/company/byteBox.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class byteBox {
+    private static int getNexBallSteps(ArrayList<Boolean> iBoxes, int targetPlace) {
+        for (int i = 0; i <= iBoxes.lastIndexOf(true); i++) {
+            if (iBoxes.get(i).equals(true)) {
+                iBoxes.set(i, false);
+                return Math.abs(i - targetPlace);
+            }
+        }
+        return -1;
+    }
+
+    private static int getNexBoxSteps(ArrayList<Boolean> iBoxesOriginal, int targetPlace) {
+        ArrayList<Boolean> iBoxes = new ArrayList<>(iBoxesOriginal);
+        int steps = 0;
+        int flag;
+        for (int i = 0; i < iBoxes.lastIndexOf(true); i++) {
+            flag = getNexBallSteps(iBoxes, targetPlace);
+            if (flag == -1) {
+                break;
+            } else {
+                steps += flag;
+            }
+        }
+        return steps;
+    }
+
+    private static void toArray(String sBoxes, ArrayList<Boolean> iBoxes) {
+        for (int i = 0; i < sBoxes.length(); i++) {
+            if (sBoxes.charAt(i) == '1') {
+                iBoxes.add(true);
+            } else {
+                iBoxes.add(false);
+            }
+        }
+    }
+
+    private static void viewOperations(int[] minOperations, ArrayList<Boolean> iBoxes) {
+        int length = Math.max(iBoxes.lastIndexOf(true), iBoxes.lastIndexOf(false));
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (j == i) {
+                    if (iBoxes.get(j).equals(true))
+                        System.out.print(">1");
+                    else
+                        System.out.print(">0");
+                } else if (iBoxes.get(j).equals(true))
+                    System.out.print(" 1");
+                else
+                    System.out.print(" 0");
+            }
+            System.out.print("  " + minOperations[i]);
+            System.out.println();
+        }
+    }
+
+    public static int[] getMinOperations(String sBoxes) {
+        int[] minOperations = new int[sBoxes.length()];
+        ArrayList<Boolean> iBoxes = new ArrayList<>();
+
+        toArray(sBoxes, iBoxes);
+
+        for (int i = 0; i < sBoxes.length(); i++) {
+            minOperations[i] = getNexBoxSteps(iBoxes, i);
+        }
+
+        boolean allowViewOperations = true;
+        if (allowViewOperations)
+            viewOperations(minOperations, iBoxes);
+
+        return minOperations;
+    }
+
+    public static void main(String[] args) {
+        int[] minOperations = getMinOperations("001011");
+        System.out.println(Arrays.toString(minOperations));
+    }
+}
